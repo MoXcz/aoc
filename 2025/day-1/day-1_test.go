6ffb8176bd67b2eb2cab5151b8dfe1a6ae51ac45
MoXcz <130593875+MoXcz@users.noreply.github.com>
2025-12-02 19:04:12 -0600

    simplify tests

--- 2025/day-1/day-1_test.go
+++ 2025/day-1/day-1_test.go
@@ -2,74 +2,18 @@ package main
 
 import "testing"
 
-func Test_runDial(t *testing.T) {
-	tests := []struct {
-		name     string
-		dial     int
-		filename string
-		want     int
-	}{
-		{
-			name:     "Example input",
-			dial:     50,
-			filename: "test.txt",
-			want:     6,
-		},
-		{
-			name:     "Test right count of 0",
-			dial:     50,
-			filename: "test_2.txt",
-			want:     10,
-		},
-		{
-			name:     "Negative count",
-			dial:     50,
-			filename: "test_3.txt",
-			want:     3,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			clicks := runDial(tt.dial, 0, tt.filename)
-			if tt.want != clicks {
-				t.Errorf("got = %d, want = %d", clicks, tt.want)
-			}
-		})
+func TestPart1(t *testing.T) {
+	want := 50
+	clicks := runDialPart1(50, 0, "test.txt")
+	if clicks != 3 {
+		t.Errorf("got = %d, want = %d", clicks, want)
 	}
 }
 
-func Test_runDialPart1(t *testing.T) {
-	tests := []struct {
-		name     string
-		dial     int
-		filename string
-		want     int
-	}{
-		{
-			name:     "Example input",
-			dial:     50,
-			filename: "test.txt",
-			want:     3,
-		},
-		{
-			name:     "Test right count of 0",
-			dial:     50,
-			filename: "test_2.txt",
-			want:     0,
-		},
-		{
-			name:     "Negative count",
-			dial:     50,
-			filename: "test_3.txt",
-			want:     1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			clicks := runDialPart1(tt.dial, 0, tt.filename)
-			if tt.want != clicks {
-				t.Errorf("got = %d, want = %d", clicks, tt.want)
-			}
-		})
+func TestPart2(t *testing.T) {
+	want := 50
+	clicks := runDial(50, 0, "test.txt")
+	if clicks != 6 {
+		t.Errorf("got = %d, want = %d", clicks, want)
 	}
 }
